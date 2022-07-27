0b6569d087fa4d1643d60d5e7b5f4a9c84cdecf6
NashuhaIsmail <103544279+NashuhaIsmail@users.noreply.github.com>
2022-07-27 10:57:42 +0800

    Update cc_monitoring.py

--- cc_monitoring.py
+++ cc_monitoring.py
@@ -87,7 +87,7 @@ def clean_email(add_task):
     add_task['Email Address'] = [x.replace('@invokeisdata.com', '') for x in df['Email Address']]
     add_task['Email Address'] = [x.replace('hudahusna', 'huda') for x in df['Email Address']]
     add_task['Email Address'] = [x.replace('amishaa', 'amisha') for x in df['Email Address']]
-   add_task['Email Address'] = [x.replace('athiyah', 'tiyah') for x in df['Email Address']]
+    add_task['Email Address'] = [x.replace('athiyah', 'tiyah') for x in df['Email Address']]
     add_task['Email Address'] = [x.replace('atiqahliyana', 'atiqah') for x in df['Email Address']]
     return add_task 
 
