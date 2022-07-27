d1fc889d7214c2022574e623c621d594f0b84c0a
NashuhaIsmail <103544279+NashuhaIsmail@users.noreply.github.com>
2022-07-27 10:59:46 +0800

    Update cc_monitoring.py

--- cc_monitoring.py
+++ cc_monitoring.py
@@ -83,12 +83,12 @@ def clean_email(add_task):
     returns dataframe with cleaned names
     this needed when to join the file
     '''
-    add_task['Email Address'] = [x.strip() for x in df['Email Address']]
-    add_task['Email Address'] = [x.replace('@invokeisdata.com', '') for x in df['Email Address']]
-    add_task['Email Address'] = [x.replace('hudahusna', 'huda') for x in df['Email Address']]
-    add_task['Email Address'] = [x.replace('amishaa', 'amisha') for x in df['Email Address']]
-    add_task['Email Address'] = [x.replace('athiyah', 'tiyah') for x in df['Email Address']]
-    add_task['Email Address'] = [x.replace('atiqahliyana', 'atiqah') for x in df['Email Address']]
+    add_task['Email Address'] = [x.strip() for x in add_task['Email Address']]
+    add_task['Email Address'] = [x.replace('@invokeisdata.com', '') for x in add_task['Email Address']]
+    add_task['Email Address'] = [x.replace('hudahusna', 'huda') for x in add_task['Email Address']]
+    add_task['Email Address'] = [x.replace('amishaa', 'amisha') for x in add_task['Email Address']]
+    add_task['Email Address'] = [x.replace('athiyah', 'tiyah') for x in add_task['Email Address']]
+    add_task['Email Address'] = [x.replace('atiqahliyana', 'atiqah') for x in add_task['Email Address']]
     return add_task 
 
     ''''
