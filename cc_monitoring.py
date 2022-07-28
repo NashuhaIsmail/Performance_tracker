4130cd155d1ed9c51c1c02f76ccbf391564af00c
NashuhaIsmail <103544279+NashuhaIsmail@users.noreply.github.com>
2022-07-28 10:52:28 +0800

    Update cc_monitoring.py

--- cc_monitoring.py
+++ cc_monitoring.py
@@ -186,15 +186,15 @@ if option1 == 'Daily':
                             '''
                             
                     if cat == 'A':
-                            df['Points'] =float( (df['Calls-CR'] * 10/60) + (df['CR'] * 5) + (df['Ex_point']))
+                            df['Points'] =(df['Calls-CR'] * 10/60) + (df['CR'] * 5) + (df['Ex_point'])
                     elif cat == 'B':
-                            df['Points'] = float((df['Calls-CR'] * 10/60) + (df['CR'] * 10)+ (df['Ex_point']))
+                            df['Points'] =(df['Calls-CR'] * 10/60) + (df['CR'] * 10)+ (df['Ex_point'])
                     elif cat == 'C':
-                                df['Points'] =float((df['Calls-CR'] * 10/50) + (df['CR'] * 10)+ (df['Ex_point']))
+                                df['Points'] =(df['Calls-CR'] * 10/50) + (df['CR'] * 10)+ (df['Ex_point'])
                     elif cat == 'D':
-                                df['Points'] =float (df['Calls-CR'] * 0.25) + (df['CR'] * 20)+ (df['Ex_point'])
+                                df['Points'] =(df['Calls-CR'] * 0.25) + (df['CR'] * 20)+ (df['Ex_point'])
                     else:
-                                df['Points'] =float((df['Calls-CR'] * 25/80) + (df['CR'] * 25)+ (df['Ex_point']))
+                                df['Points'] =(df['Calls-CR'] * 25/80) + (df['CR'] * 25)+ (df['Ex_point'])
                                                 
                     df = df[['Agent', 'Calls Attempted', 'CR', 'Points', 'Average Call Dur (s)']]
                     dfs.append(df)
