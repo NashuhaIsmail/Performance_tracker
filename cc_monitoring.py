70d928792da3b4de30b38f7da12d996eb0373eda
NashuhaIsmail <103544279+NashuhaIsmail@users.noreply.github.com>
2022-07-28 10:30:59 +0800

    Update cc_monitoring.py

--- cc_monitoring.py
+++ cc_monitoring.py
@@ -205,7 +205,7 @@ if option1 == 'Daily':
         agents = list(df['Agent'].unique())
         calls = [sum(df[df['Agent'] == x]['Calls Attempted']) for x in agents]
         crs = [sum(df[df['Agent'] == x]['CR']) for x in agents]
-        points = [sum(df[df['Agent'] == x]['Points']) for x in agents]
+        points =float( [sum(df[df['Agent'] == x]['Points']) for x in agents])
         avg_dur = [sum(df[df['Agent'] == x]['Average Call Dur (s)']) / len(df[df['Agent'] == x]) for x in agents]
         df = pd.DataFrame({'Agent': agents,
                             'Calls Attempted': calls,
